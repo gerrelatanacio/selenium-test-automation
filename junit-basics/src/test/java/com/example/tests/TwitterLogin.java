ecb61156307da620b9266106389bd0c1bbe6ebb3
gerrelatanacio <gerrelatanacio@gmail.com>
2020-01-20 11:00:51 +0800

    junit first commit

---
+++ junit-basics/src/test/java/com/example/tests/TwitterLogin.java
@@ -0,0 +1,87 @@
+package com.example.tests;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TwitterLogin {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+	WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTwitterLogin() throws Exception {
+    driver.get("https://twitter.com/?lang=en");
+    driver.findElement(By.name("session[username_or_email]")).click();
+    driver.findElement(By.name("session[username_or_email]")).clear();
+    driver.findElement(By.name("session[username_or_email]")).sendKeys("gerrelatanacio@ymail.com");
+    driver.findElement(By.name("session[password]")).click();
+    driver.findElement(By.name("session[password]")).clear();
+    driver.findElement(By.name("session[password]")).sendKeys("05Naruto24~");
+    driver.findElement(By.name("session[password]")).sendKeys(Keys.ENTER);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}

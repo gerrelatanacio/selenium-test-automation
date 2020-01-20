a8dbcc1848dff61981810ff94b85d9fbcb5b17a5
Gerrel <gerrel.atanacio@delta.com>
2020-01-20 08:15:46 -0500

    committing new changes

---
+++ testng-basics/src/test/java/com/example/tests/FirstSeleniumTestNGTest.java
@@ -0,0 +1,55 @@
+package com.example.tests;
+
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstSeleniumTestNGTest {
+	WebDriver driver;
+
+	@BeforeTest
+	public void before() {
+		WebDriverManager.chromedriver().setup();
+		// Create an instance of the webdriver
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void test() {
+		driver.get("https://www.google.com");
+
+		System.out.println(driver.getCurrentUrl());
+
+		System.out.println(driver.getTitle());
+
+		assertEquals(driver.getTitle(), "Google");
+
+	}
+
+	@Test
+	public void testFacebook() {
+		driver.get("https://www.facebook.com");
+
+		System.out.println(driver.getCurrentUrl());
+
+		System.out.println(driver.getTitle());
+
+		String title = "Facebook - Log In or Sign Up";
+
+		assertEquals(driver.getTitle(), title);
+
+	}
+	
+	@AfterTest
+	public void after() {
+		driver.quit();
+	}
+
+}
